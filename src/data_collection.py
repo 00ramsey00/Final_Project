3ee07a1f3c8f3b0487b4ba732ed8a792e8795d70
Ramsey <rbasma@usc.edu>
2025-11-10 13:50:58 -0800

    added mortage rate data pull code with FRED to data_collection

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,10 +1,27 @@
 import os
-os.environ["KAGGLE_CONFIG_DIR"] = r"C:\Users\rpbas\Desktop\Fall 2025 DSCI 510\Final Project\Final_Project_Ramsey_Basma\Final_Project"
-import kaggle
 import pandas as pd
+from dotenv import load_dotenv
+#os.environ["KAGGLE_CONFIG_DIR"] = r"C:\Users\rpbas\Desktop\Fall 2025 DSCI 510\Final Project\Final_Project_Ramsey_Basma\Final_Project\src"
+
+# Load environment variables from .env file
+load_dotenv()  # By default it loads from a file named .env in the same folder
+
+# Set Kaggle config directory from the .env file
+kaggle_config_dir = os.getenv("KAGGLE_CONFIG_DIR")
+if not kaggle_config_dir:
+    raise ValueError("KAGGLE_CONFIG_DIR is not set in the .env file")
+
+# Normalize path for Windows/Mac/Linux
+kaggle_config_dir = os.path.normpath(kaggle_config_dir)
+os.environ["KAGGLE_CONFIG_DIR"] = kaggle_config_dir
+
+print("Using Kaggle config dir:", kaggle_config_dir)
+
+import kaggle
+
 
 # --- 1. Download Zillow dataset from Kaggle ---
-dataset = 'zillow/zecon'
+dataset = 'ahmedshahriarsakib/usa-real-estate-dataset'
 extract_dir = 'zillow_data'
 os.makedirs(extract_dir, exist_ok=True)
 
